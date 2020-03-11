41ae3d1e03e286d2bcbe05718e9e00bcae889fa7
charliePaguay <charlie.paguay@gmail.com>
2020-03-11 09:36:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# paguaydemo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/paguaydemo)
---
+++ src/app/body/body.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-body",
+  templateUrl: "./body.component.html"
+})
+export class BodyComponent {
+
+mostrar=true;
+
+  frase: any = {
+    mensaje: "un gran poder llevar una gran responsabilidad",
+    autor: "Ben Parker"
+  };
+
+  personajes: string [] =['spiderman','venom','charlie']
+}
